937f7160a90510e4316ebde3963367026073a10d
Iryna Melnyk <melnykirina1@gmail.com>
2024-02-20 19:02:20 +0100

    Division is supported

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ def subtract(x, y):
     return x - y
 
 
+def divide(x, y):
+    return x / y
+
+
 def app():
     if len(sys.argv) != 4:
-        sys.exit(1)
+        return "Invalid operation: Please provide three arguments"
 
     operation = sys.argv[1]
     num1 = float(sys.argv[2])
@@ -19,12 +23,16 @@ def app():
 
     if operation == "add":
         result = add(num1, num2)
-        print(f"Result of adding {num1} and {num2}: {result}")
+        return f"Result of adding {num1} and {num2}: {result}"
     elif operation == "subtract":
         result = subtract(num1, num2)
-        print(f"Result of subtracting {num2} from {num1}: {result}")
+        return f"Result of subtracting {num2} from {num1}: {result}"
+    elif operation == "divide":
+        result = divide(num1, num2)
+        return f"Result of division {num1} by {num2}: {result}"
     else:
-        print("Invalid operation")
+        return "Invalid operation: Please choose 'add', 'subtract', or 'divide'"
 
 
-app()
+result = app()
+print(result)
